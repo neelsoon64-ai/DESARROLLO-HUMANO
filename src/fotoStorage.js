57d1b2a9d6dbb354f7aa0c4acf34252895d2f705
Nelson Esteban Fuentes <neelsoon64@gmail.com>
2026-07-01 12:59:59 -0300

    actualizacion

--- src/fotoStorage.js
+++ src/fotoStorage.js
@@ -10,7 +10,7 @@ export function generarPreviewDesdeArchivo(file) {
   });
 }
 
-const PUENTE_DRIVE_URL = import.meta.env.VITE_GOOGLE_DRIVE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbyOOOlVQwsAQLsKnYtWL2OA7MroSjOstUkqT9ERSDCNe3yN23uyE5mAhIKxR0rzbTI0/exec";
+const PUENTE_DRIVE_URL = import.meta.env.VITE_GOOGLE_DRIVE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbzl3eDGV--gvrB1NFJpdio0W2aHSBH9GhKjtntUMHn3bu3nVD2v6OrbspoKvNy9omTK/exec";
 
 function extraerBase64DesdeDataUrl(dataUrl) {
   const match = /^data:(image\/[^;]+);base64,(.*)$/i.exec(dataUrl);
@@ -126,7 +126,7 @@ export async function eliminarFotoRemito(url) {
     }
   }
 
-  const URL_PUENTE_DRIVE = "https://script.google.com/macros/s/AKfycbyOOOlVQwsAQLsKnYtWL2OA7MroSjOstUkqT9ERSDCNe3yN23uyE5mAhIKxR0rzbTI0/exec";
+  const URL_PUENTE_DRIVE = import.meta.env.VITE_GOOGLE_DRIVE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbzl3eDGV--gvrB1NFJpdio0W2aHSBH9GhKjtntUMHn3bu3nVD2v6OrbspoKvNy9omTK/exec";
 
   try {
     const payload = { action: "delete", url };
