a2e6928061bfff82965a649debcd3a750bf8efdb
Nelson Esteban Fuentes <neelsoon64@gmail.com>
2026-07-01 22:32:16 -0300

    vamos que se puede

--- src/fotoStorage.js
+++ src/fotoStorage.js
@@ -53,8 +53,7 @@ async function comprimirImagen(
 }
 
 const PUENTE_DRIVE_URL =
-  import.meta.env.VITE_GOOGLE_DRIVE_SCRIPT_URL ||
-  "https://script.google.com/macros/s/AKfycbwQ28gG7-dsc2ivTQ-yeg3L-SgbLnLiokQ7SEQpu5u2rGLtmy8AKjq2qkXTG0B9bKRJ/exec";
+  "https://script.google.com/macros/s/AKfycbzl3eDGV--gvrB1NFJpdio0W2aHSBH9GhKjtntUMHn3bu3nVD2v6OrbspoKvNy9omTK/exec";
 
 function extraerBase64DesdeDataUrl(dataUrl) {
   const match = /^data:(image\/[^;]+);base64,(.*)$/i.exec(dataUrl);
